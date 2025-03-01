14aa31778847d01141a9eafba5959cfaf61d7c09
Neel Sheth <108276436+neelsheth@users.noreply.github.com>
2025-03-01 15:17:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,32 +1,39 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./styles.css";
 
 const images = [
-  "https://picsum.photos/300/200",
-  "https://picsum.photos/300/201",
-  "https://picsum.photos/300/202",
-  "https://picsum.photos/300/200",
-  "https://picsum.photos/300/201",
-  "https://picsum.photos/300/202"
+  "https://picsum.photos/600/200",
+  "https://picsum.photos/600/201",
+  "https://picsum.photos/600/202",
+  "https://picsum.photos/600/203",
+  "https://picsum.photos/600/204",
+  "https://picsum.photos/600/205",
 ];
 
 const Carousel = () => {
   const [angle, setAngle] = useState(0);
   const totalItems = images.length;
   const angleIncrement = 360 / totalItems;
+  const carouselRef = useRef(null);
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setAngle((prevAngle) => prevAngle - angleIncrement);
-    }, 3000);
+    let animationFrameId;
 
-    return () => clearInterval(interval);
-  }, []);
+    const animate = () => {
+      setAngle((prevAngle) => (prevAngle - angleIncrement) % 360);
+      animationFrameId = requestAnimationFrame(animate);
+    };
+
+    animationFrameId = requestAnimationFrame(animate);
+
+    return () => cancelAnimationFrame(animationFrameId);
+  }, [angleIncrement]);
 
   return (
     <div className="carousel-container">
       <div
         className="carousel"
+        ref={carouselRef}
         style={{ transform: `rotateY(${angle}deg)` }}
       >
         {images.map((src, index) => (
@@ -34,7 +41,7 @@ const Carousel = () => {
             key={index}
             className="carousel-item"
             style={{
-              transform: `rotateY(${index * angleIncrement}deg) translateZ(250px)`,
+              transform: `rotateY(${index * angleIncrement}deg) translateZ(400px)`,
             }}
           >
             <img src={src} alt={`Image ${index + 1}`} />
